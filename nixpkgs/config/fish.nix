3c52038dfee27c6dce793d72e88b24a07095b3cc
m00p1ng <mongkonchai4412@gmail.com>
2022-04-11 03:37:23 +0700

    add wip command

--- nixpkgs/config/fish.nix
+++ nixpkgs/config/fish.nix
@@ -47,15 +47,12 @@
     shellAliases = {
       vim = "nvim";
       mv = "mv -i";
-      gl = "git pull";
-      gp = "git push";
-      gpo = "git push -u origin HEAD";
       glog = "git log --oneline --decorate --graph";
       gloga = "git log --oneline --decorate --graph --all";
       glg = "git log --stat";
       glgp = "git log --stat -p";
       gwip = "git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m \"--wip-- [skip ci]\"";
-      gunwip = "git log -n 1 | grep -q -c \"\-\-wip\-\-\" && git reset HEAD~1";
+      gunwip = "git log -n 1 | grep -q -c \"\\-\\-wip\\-\\-\" && git reset HEAD~1";
       gignore = "git update-index --assume-unchanged";
       gignored = "git ls-files -v | grep \"^[[:lower:]]\"";
       gunignore = "git update-index --no-assume-unchanged";
@@ -66,11 +63,12 @@
     };
     shellAbbrs = {
       g = "git";
-      gff = "git fuzzy";
+      gl = "git pull";
+      gp = "git push";
+      gpo = "git push -u origin HEAD";
     };
     functions = {
       fish_prompt = ''
-        set -l last_pipestatus $pipestatus
         set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
 
         # git set up
@@ -91,7 +89,7 @@
         set -g __fish_git_prompt_char_upstream_prefix ' '
 
         echo
-        if [ $last_pipestatus -eq 0 ]
+        if [ $__fish_last_status -eq 0 ]
           set_color --background 5FAFAF FFFFFF
         else
           set_color --background FF6D67 FFFFFF
@@ -100,7 +98,15 @@
         set_color --background normal normal; echo -n " : "
         set_color AFFF00; echo -n (prompt_pwd)
 
-        printf '%s' (fish_vcs_prompt)
+        printf '%s' (fish_git_prompt)
+
+        set -lx __git_output (fish_git_prompt)
+        if [ (count $__git_output) -eq 1 ]
+          set -lx __git_wip (git log -n 1 | grep "\-\-wip\-\-")
+          if [ (count $__git_wip) -eq 1 ]
+            printf "%s WIP!!!" (set_color FF6D67)
+          end
+        end
 
         # traffic light arrow
         printf "\n%s▶%s▶%s▶ " (set_color FF6D67) (set_color FEFB67) (set_color 56FF5F)
