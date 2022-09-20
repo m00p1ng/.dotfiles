1ba619886e3c5ae1eaa41fcab84ad4cd05c9a5e6
m00p1ng <mongkonchai4412@gmail.com>
2022-09-20 16:33:18 +0700

    update script

--- configs/fish/functions/home_config_apply.fish
+++ configs/fish/functions/home_config_apply.fish
@@ -1,17 +1,21 @@
-function home_config_apply
-  if test (count $argv) != 1
-    echo Please provide name
+function home_config_apply -a profile
+  if not test "$profile"
+    echo Please provide configuration profile
     return 1
   end
+
+  argparse 'no-update' -- $argv
 
   pushd .
   cd ~/.dotfiles
   gunignore ./modules/user.nix
   gwip 1> /dev/null
 
-  nix flake update
-  nix flake lock --update-input home-manager
-  home-manager switch --flake ~/.dotfiles#$argv[1]
+  if not test "$_flag_no_update"
+    nix flake update
+    nix flake lock --update-input home-manager
+  end
+  home-manager switch --flake ~/.dotfiles#$profile
 
   gunwip 1> /dev/null
   gignore ./modules/user.nix
