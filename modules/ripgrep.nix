15a9d47c162fafb62c4058c1f1094401a440b5d4
m00p1ng <mongkonchai4412@gmail.com>
2022-10-10 12:18:25 +0700

    refactor ripgrep

--- modules/ripgrep.nix
+++ modules/ripgrep.nix
@@ -9,7 +9,17 @@
     RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/.ripgreprc";
   };
 
-  xdg.configFile."ripgrep/.ripgreprc" = {
-    source = ../configs/ripgrep/.ripgreprc;
-  };
+  xdg.configFile."ripgrep/.ripgreprc".text = ''
+    --type-add
+    web:*.{html,css,scss,sass,vue,js,jsx,ts,tsx}*
+
+    --colors=line:fg:yellow
+    --colors=line:style:bold
+
+    --colors=path:fg:green
+    --colors=path:style:bold
+
+    --glob=!.git/*
+    --hidden
+  '';
 }
