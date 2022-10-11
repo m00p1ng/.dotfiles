261816fa2c6e19c549d86c45638b1c2c9573d124
m00p1ng <mongkonchai4412@gmail.com>
2022-10-11 19:19:54 +0700

    update fnm

--- modules/fnm.nix
+++ modules/fnm.nix
@@ -7,6 +7,9 @@
 
   xdg.configFile."fish/conf.d/fnm.fish".text = ''
     fnm env --use-on-cd | source
+  '';
+
+  xdg.configFile."fish/completions/fnm.fish".text = ''
     fnm completions --shell fish | source
   '';
 }
