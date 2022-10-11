82c707d7bd522a06b8b2637ce6e5204bf71de9fd
m00p1ng <mongkonchai4412@gmail.com>
2022-10-11 22:18:51 +0700

    add execution_timer

---
+++ configs/fish/conf.d/execution_timer.fish
@@ -0,0 +1,47 @@
+function join_space
+  if [ "$argv[1]" = "" ]
+    echo "$argv[2]"
+  else
+    echo "$argv[1] $argv[2]"
+  end
+end
+
+function execution_timer -e fish_postexec
+  set -l SEC 1000
+  set -l MIN 60000
+  set -l HOUR 3600000
+  set -l DAY 86400000
+  set -l MIN_TIMER (math -s0 "5 * $SEC")
+  set -l total_time $CMD_DURATION
+
+  if [ $total_time -lt $MIN_TIMER ]
+    return
+  end
+
+  set -l days (math -s0 "$total_time / $DAY")
+  set -l hours (math -s0 "$total_time % $DAY / $HOUR")
+  set -l mins (math -s0 "$total_time % $HOUR / $MIN")
+  set -l secs (math -s0 "$total_time % $MIN / $SEC")
+
+  set -l output ""
+
+  if [ $days -ne 0 ]
+    set output "$days"d
+  end
+
+  if [ $hours -ne 0 ]
+    set output (join_space $output "$hours"s)
+  end
+
+  if [ $mins -ne 0 ]
+    set output (join_space $output "$mins"m)
+  end
+
+  if [ $secs -ne 0 ]
+    set output (join_space $output "$secs"s)
+  end
+
+  set_color -b 303030
+  echo -e "\n tooks $output "
+  set_color normal
+end
