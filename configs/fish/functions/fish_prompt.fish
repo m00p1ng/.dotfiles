9886a3f0a2aac71baca316583ef3ff8233ebbd4b
m00p1ng <mongkonchai4412@gmail.com>
2023-02-13 11:47:38 +0700

    update fish

--- configs/fish/functions/fish_prompt.fish
+++ configs/fish/functions/fish_prompt.fish
@@ -74,7 +74,7 @@ function get_left_prompt
   set -g __fish_git_prompt_show_informative_status 1
   set -g __fish_git_prompt_hide_untrackedfiles 1
   set -g __fish_git_prompt_showupstream informative
-  set -g __fish_git_prompt_showstashstate informative
+  set -g __fish_git_prompt_showstashstate 1
   set -g __fish_git_prompt_char_stateseparator ' '
   set -g __fish_git_prompt_char_upstream_prefix ' '
 
