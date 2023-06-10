37e1659d944f15ac16e0ca786bcc29e301ccedec
m00p1ng <mongkonchai4412@gmail.com>
2023-06-10 22:20:59 +0700

    update

--- modules/fish.nix
+++ modules/fish.nix
@@ -13,7 +13,7 @@
       glg = "git log --stat";
       glgp = "git log --stat -p";
       gwip = "git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m \"--wip-- [skip ci]\"";
-      gunwip = "git log -n 1 | grep -q -c \"\\-\\-wip\\-\\-\" && git reset HEAD~1";
+      gunwip = "git log -n 1 | grep -q -c -e \"--wip--\" && git reset HEAD~1";
       gignore = "git update-index --skip-worktree";
       gignored = "git ls-files -v | grep \"^S\"";
       gunignore = "git update-index --no-skip-worktree";
