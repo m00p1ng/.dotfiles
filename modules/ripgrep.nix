d21c40f7e0adbc8606d9acb4fd4e359ef0d6316f
m00p1ng <mongkonchai4412@gmail.com>
2023-12-12 10:52:32 +0700

    update ripgrep

--- modules/ripgrep.nix
+++ modules/ripgrep.nix
@@ -1,25 +1,17 @@
-{ pkgs, config, ... }:
-
 {
-  home.packages = with pkgs; [
-    ripgrep
-  ];
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--type-add=web:*.{html,css,scss,sass,vue,js,jsx,ts,tsx}*"
 
-  home.sessionVariables = {
-    RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/.ripgreprc";
+      "--colors=line:fg:yellow"
+      "--colors=line:style:bold"
+
+      "--colors=path:fg:green"
+      "--colors=path:style:bold"
+
+      "--glob=!.git/"
+      "--hidden"
+    ];
   };
-
-  xdg.configFile."ripgrep/.ripgreprc".text = ''
-    --type-add
-    web:*.{html,css,scss,sass,vue,js,jsx,ts,tsx}*
-
-    --colors=line:fg:yellow
-    --colors=line:style:bold
-
-    --colors=path:fg:green
-    --colors=path:style:bold
-
-    --glob=!.git/
-    --hidden
-  '';
 }
