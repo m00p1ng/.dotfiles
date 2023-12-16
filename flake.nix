bf77f735be3b20a52cbf1bd2afa933ce3b6de56b
m00p1ng <mongkonchai4412@gmail.com>
2023-12-16 14:53:33 +0700

    fix unfree

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs
-    home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
@@ -44,6 +44,7 @@
           ./modules/zoxide.nix
           ./overridden.nix
           ({ pkgs, ... }: {
+            nixpkgs.config.allowUnfree = true;
             home.packages = with pkgs; [
               awscli
               # cocoapods
