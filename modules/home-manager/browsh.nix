12de84a5cf88d1e791dd4673693b54bf15b943c1
m00p1ng <mongkonchai4412@gmail.com>
2023-12-25 21:12:12 +0700

    update

--- modules/home-manager/browsh.nix
+++ modules/home-manager/browsh.nix
@@ -8,12 +8,15 @@ in
 {
   options.programs.browsh = {
     enable = mkEnableOption "browsh, A fully-modern text-based browser, rendering to TTY and browsers";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.browsh;
+    };
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      browsh
-    ];
+    home.packages = [ cfg.package ];
 
     # xdg.configFile."browsh/config.toml".text = ''
     # '';
