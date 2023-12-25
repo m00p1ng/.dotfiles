baf7429ad7fc401e7f1324076eb7814416312c1c
m00p1ng <mongkonchai4412@gmail.com>
2023-12-25 11:21:08 +0700

    update

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -19,6 +19,16 @@
     remapCapsLockToControl = true;
   };
 
+  fonts.fontDir.enable =
+    true; # if this is true, manually installed system fonts will be deleted!
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override {
+      fonts = [
+        "JetBrainsMono"
+      ];
+    })
+  ];
+
   # allow touchid to auth sudo -- this comes from pam.nix, which needs to be loaded before this
   # it's now standard to nix-darwin, but without the special such needed for tmux, so we
   # will continue using our custom script
