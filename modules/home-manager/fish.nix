a092b51c879c9eb524c27b71cbd379197642ea80
m00p1ng <mongkonchai4412@gmail.com>
2023-12-26 21:44:33 +0700

    update

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -4,86 +4,67 @@ with lib;
 let
   cfg = config.programs.fish;
 in {
-  config = mkMerge([
-    {
-      programs.fish = {
-        shellAliases = {
-          "..." = "cd ../..";
-          vim = "nvim";
-          mv = "mv -i";
+  config = mkIf cfg.enable {
+    programs.fish = {
+      shellAliases = {
+        "..." = "cd ../..";
+        vim = "nvim";
+        mv = "mv -i";
 
-          # git
-          gc = "git commit";
-          glog = "git log --oneline --decorate --graph";
-          gloga = "git log --oneline --decorate --graph --all";
-          glg = "git log --stat";
-          glgp = "git log --stat -p";
-          gwip = "git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m \"--wip-- [skip ci]\"";
-          gunwip = "git log -n 1 | grep -q -c -e \"--wip--\" && git reset HEAD~1";
-          gignore = "git update-index --skip-worktree";
-          gignored = "git ls-files -v | grep \"^S\"";
-          gunignore = "git update-index --no-skip-worktree";
-          gcount = "git shortlog -sn";
+        s = "kitty +kitten ssh";
+        check_disk = "sudo smartctl --all /dev/disk0s1";
+        isodate = "date +%Y-%m-%d";
+        isodatetime = "date +\"%Y-%m-%dT%H:%M:%S\"";
 
-          s = "kitty +kitten ssh";
-          check_disk = "sudo smartctl --all /dev/disk0s1";
-          isodate = "date +%Y-%m-%d";
-          isodatetime = "date +\"%Y-%m-%dT%H:%M:%S\"";
+        # network
+        ip = "curl api.ipify.org";
+        watchdns = "sudo tcpdump -vvi any port 53";
+        cleardns = "sudo killall -HUP mDNSResponder";
+        wifi-network-name = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'";
+        wifi-password = "security find-generic-password -wa (wifi-network-name)";
+        wifi-reset = "networksetup -setairportpower en0 off && networksetup -setairportpower en0 on";
 
-          # network
-          ip = "curl api.ipify.org";
-          watchdns = "sudo tcpdump -vvi any port 53";
-          cleardns = "sudo killall -HUP mDNSResponder";
-          wifi-network-name = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'";
-          wifi-password = "security find-generic-password -wa (wifi-network-name)";
-          wifi-reset = "networksetup -setairportpower en0 off && networksetup -setairportpower en0 on";
-
-          # macos
-          showfiles = "defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder";
-          hidefiles = "defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder";
-          icloud = "cd ~/Library/Mobile\\ Documents/com\~apple\~CloudDocs";
-        };
-        shellAbbrs = {
-          g = "git";
-          gl = "git pull";
-          gp = "git push";
-          gpf = "git push --force-with-lease";
-          gpo = "git push -u origin HEAD";
-          gco = "git checkout";
-          "-" = "cd -"; # abbr -a -- - 'cd -'
-        };
-        shellInit = ''
-          fish_add_path ~/.nix-profile/bin
-          fish_add_path /nix/var/nix/profiles/default/bin
+        # macos
+        showfiles = "defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder";
+        hidefiles = "defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder";
+        icloud = "cd ~/Library/Mobile\\ Documents/com\~apple\~CloudDocs";
+      };
+      shellAbbrs = {
+        "-" = "cd -"; # abbr -a -- - 'cd -'
+      };
+      shellInit = ''
+        fish_add_path ~/.nix-profile/bin
+        fish_add_path /nix/var/nix/profiles/default/bin
+        ${optionalString pkgs.stdenv.hostPlatform.isDarwin ''
           fish_add_path /run/current-system/sw/bin
-        '';
-        interactiveShellInit = ''
-          bind \ep history-token-search-backward
-          bind \en history-token-search-forward
+        ''}
+      '';
+      interactiveShellInit = ''
+        bind \ep history-token-search-backward
+        bind \en history-token-search-forward
 
+        ${optionalString pkgs.stdenv.hostPlatform.isDarwin ''
           for f in ${config.xdg.configHome}/fish/functions/macos/*.fish
             source $f
           end
-        '';
-        plugins = with pkgs; [
-          # {
-          #   name = "fish-functions";
-          #   src = fetchFromGitHub {
-          #     owner = "razzius";
-          #     repo = "fish-functions";
-          #     rev = "11dea7268cecd25d8ec17862917d5dba338af5eb";
-          #     hash = "sha256-eyDZaZlqtRPhn08tTFYXthkMhVFX0yN6eXIOMoS6RWk=";
-          #   };
-          # }
-        ];
-      };
-    }
+        ''}
+      '';
+      plugins = with pkgs; [
+        # {
+        #   name = "fish-functions";
+        #   src = fetchFromGitHub {
+        #     owner = "razzius";
+        #     repo = "fish-functions";
+        #     rev = "11dea7268cecd25d8ec17862917d5dba338af5eb";
+        #     hash = "sha256-eyDZaZlqtRPhn08tTFYXthkMhVFX0yN6eXIOMoS6RWk=";
+        #   };
+        # }
+      ];
+    };
 
-    (mkIf cfg.enable {
-      xdg.configFile."fish" = {
-        source = ./configs/fish;
-        recursive = true;
-      };
-    })
-  ]);
+    xdg.configFile."fish" = {
+      source = ./configs/fish;
+      recursive = true;
+    };
+  };
 }
--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,105 +1,124 @@
 { pkgs, config, lib, ... }:
 
 with lib;
-
 let
   cfg = config.programs.git;
-in
-{
-  config = (mkMerge [
-    {
-      programs.git = {
-        ignores = [ ".DS_Store" ];
-        aliases = {
-          logs = "log --abbrev-commit --graph --decorate --oneline";
-          logl = "log --oneline -10";
-          logs-full = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr) %C(dim white)<%an>%C(reset)'";
-          logs-date = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr)%C(reset)'";
-          logs-author = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)%an%C(reset)'";
-          follow = "!git --no-pager log --follow --oneline";
-          count = "shortlog -ns";
-          find = "log --all --full-history --oneline";
-          parent = "!git show-branch | grep '*' | grep -v \"$(git rev-parse --abbrev-ref HEAD)\" | head -n1 | sed 's/.*\\[\\(.*\\)\\].*/\\1/' | sed 's/[\\^~].*//' #";
-          diffs = "!git -c delta.side-by-side=true diff";
-          info = "!onefetch";
+in {
+  config = mkIf cfg.enable {
+    programs.git = {
+      ignores = [ ".DS_Store" ];
+      aliases = {
+        logs = "log --abbrev-commit --graph --decorate --oneline";
+        logl = "log --oneline -10";
+        logs-full = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr) %C(dim white)<%an>%C(reset)'";
+        logs-date = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)(%cr)%C(reset)'";
+        logs-author = "log --graph --pretty='%C(yellow)%h%C(reset) -%C(auto)%d%C(reset) %s %C(green)%an%C(reset)'";
+        follow = "!git --no-pager log --follow --oneline";
+        count = "shortlog -ns";
+        find = "log --all --full-history --oneline";
+        parent = "!git show-branch | grep '*' | grep -v \"$(git rev-parse --abbrev-ref HEAD)\" | head -n1 | sed 's/.*\\[\\(.*\\)\\].*/\\1/' | sed 's/[\\^~].*//' #";
+        diffs = "!git -c delta.side-by-side=true diff";
+        info = "!onefetch";
+      };
+      extraConfig = {
+        core = {
+          editor = "nvim";
+          ignorecase = false;
         };
-        extraConfig = {
-          core = {
-            editor = "nvim";
-            ignorecase = false;
-          };
-          init = {
-            defaultBranch = "main";
-          };
-          merge = {
-            tool = "vimdiff";
-          };
-          pager = {
-            branch = false;
-            shortlog = false;
-          };
-          color = {
-            ui = true;
-            status = {
-              added = "green bold";
-              changed = "yellow bold";
-              untracked = "red bold";
-            };
-            diff = {
-              meta = "11";
-              frag = "magenta bold";
-              commit = "yellow bold";
-              old = "red bold";
-              new = "green bold";
-              whitespace = "red reverse";
-            };
-            diff-highlight = {
-              oldNormal = "red bold";
-              oldHighlight = "red bold 52";
-              newNormal = "green bold";
-              newHighlight = "green bold 22";
-            };
+        init = {
+          defaultBranch = "main";
+        };
+        merge = {
+          tool = "vimdiff";
+        };
+        pager = {
+          branch = false;
+          shortlog = false;
+        };
+        color = {
+          ui = true;
+          status = {
+            added = "green bold";
+            changed = "yellow bold";
+            untracked = "red bold";
           };
           diff = {
-            colorMoved = "default";
+            meta = "11";
+            frag = "magenta bold";
+            commit = "yellow bold";
+            old = "red bold";
+            new = "green bold";
+            whitespace = "red reverse";
           };
-          pull = {
-            rebase = true;
-          };
-          rebase = {
-            autoStash = true;
+          diff-highlight = {
+            oldNormal = "red bold";
+            oldHighlight = "red bold 52";
+            newNormal = "green bold";
+            newHighlight = "green bold 22";
           };
         };
-        delta = {
-          options = {
-            features = "line-numbers file-decorators";
-            inspect-raw-lines = false;
-            syntax-theme = "Visual Studio Dark+";
-            line-numbers = {
-              line-numbers-zero-style = "243";
-              line-numbers-left-style = "243";
-              line-numbers-right-style = "243";
-              line-numbers-minus-style = "red";
-              line-numbers-plus-style = "green";
-            };
-            file-decorators = {
-              file-style = "bold yellow";
-              file-modified-label = "modified:";
-              file-decoration-style = "yellow box ul";
-              hunk-header-style = "syntax";
-              hunk-header-decoration-style = "243 box";
-              commit-decoration-style = "green box ul";
-            };
+        diff = {
+          colorMoved = "default";
+        };
+        pull = {
+          rebase = true;
+        };
+        rebase = {
+          autoStash = true;
+        };
+      };
+      delta = {
+        options = {
+          features = "line-numbers file-decorators";
+          inspect-raw-lines = false;
+          syntax-theme = "Visual Studio Dark+";
+          line-numbers = {
+            line-numbers-zero-style = "243";
+            line-numbers-left-style = "243";
+            line-numbers-right-style = "243";
+            line-numbers-minus-style = "red";
+            line-numbers-plus-style = "green";
+          };
+          file-decorators = {
+            file-style = "bold yellow";
+            file-modified-label = "modified:";
+            file-decoration-style = "yellow box ul";
+            hunk-header-style = "syntax";
+            hunk-header-decoration-style = "243 box";
+            commit-decoration-style = "green box ul";
           };
         };
       };
-    }
+    };
 
-    (mkIf cfg.enable {
-      home.packages = with pkgs; [
-        onefetch
-        tig
-      ];
-    })
-  ]);
+    programs.fish = {
+      shellAliases = {
+        # git
+        gc = "git commit";
+        glog = "git log --oneline --decorate --graph";
+        gloga = "git log --oneline --decorate --graph --all";
+        glg = "git log --stat";
+        glgp = "git log --stat -p";
+        gwip = "git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m \"--wip-- [skip ci]\"";
+        gunwip = "git log -n 1 | grep -q -c -e \"--wip--\" && git reset HEAD~1";
+        gignore = "git update-index --skip-worktree";
+        gignored = "git ls-files -v | grep \"^S\"";
+        gunignore = "git update-index --no-skip-worktree";
+        gcount = "git shortlog -sn";
+      };
+      shellAbbrs = {
+        g = "git";
+        gl = "git pull";
+        gp = "git push";
+        gpf = "git push --force-with-lease";
+        gpo = "git push -u origin HEAD";
+        gco = "git checkout";
+      };
+    };
+
+    home.packages = with pkgs; [
+      onefetch
+      tig
+    ];
+  };
 }
