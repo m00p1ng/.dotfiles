f3f20c76ff3c74396e513debb7042c490f16b3cc
m00p1ng <mongkonchai4412@gmail.com>
2023-12-26 13:02:23 +0700

    update

--- overriding.nix
+++ overriding.nix
@@ -1,12 +1,14 @@
-{ pkgs, lib, ... }:
+{ lib, username, ... }:
 
 with lib;
 {
-  home.packages = with pkgs; [
-  ];
+  home-manager.users.${username} = {
+    home.packages = with pkgs; [
+    ];
 
-  programs.git = {
-    userName = "<git username>";
-    userEmail = "<git email>";
+    programs.git = {
+      userName = "<git username>";
+      userEmail = "<git email>";
+    };
   };
 }
