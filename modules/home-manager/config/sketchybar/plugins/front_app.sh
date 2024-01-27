80494a4467eca5f94bc49fa6fec3f8e0e0c59087
m00p1ng <mongkonchai4412@gmail.com>
2024-01-27 12:41:28 +0700

    update

--- modules/home-manager/config/sketchybar/plugins/front_app.sh
+++ modules/home-manager/config/sketchybar/plugins/front_app.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 if [ "$SENDER" = "front_app_switched" ]; then
-  sketchybar --set "$NAME" label="$INFO" icon.background.image="app.$INFO"
+  args=(label="$INFO")
+  case $INFO in
+    "SecurityAgent" | "coreautha")
+      ;;
+    *)
+      args+=(icon.background.image="app.$INFO")
+      ;;
+  esac
+
+  sketchybar --set "$NAME" "${args[@]}"
 fi
