6d6d0b4122b75f0b8f719ec030a253d08bb3d1ff
m00p1ng <mongkonchai4412@gmail.com>
2024-02-10 12:21:44 +0700

    update

--- hosts/m00p1ng/configuration.nix
+++ hosts/m00p1ng/configuration.nix
@@ -25,12 +25,35 @@ in {
        "zoom"
        "discord"
        "sf-symbols"
+       "grammarly-desktop"
 
        "postman"
        "docker"
 
        "vlc"
     ];
+
+    masApps = {
+      "Xcode" = 497799835;
+      "1Password for Safari" = 1569813296;
+      "Grammarly for Safari" = 1462114288;
+
+      "Pages" = 409201541;
+      "Numbers" = 409203825;
+      "Keynote" = 409183694;
+
+      "PDF Expert" = 1055273043;
+      "Kindle" = 302584613;
+      "GoodLinks" = 1474335294;
+      "Bear" = 1091189122;
+
+      "LINE" = 539883307;
+      "Spark" = 1176895641;
+      "Fantastical" = 975937182;
+
+      "PiPifier" = 1160374471;
+      "Money Pro" = 972572731;
+    };
   };
 
   services = {
