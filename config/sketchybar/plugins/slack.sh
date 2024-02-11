7adc9e1ee6317d1604429e472129d0ce57657b57
m00p1ng <mongkonchai4412@gmail.com>
2024-02-11 09:42:40 +0700

    update

--- config/sketchybar/plugins/slack.sh
+++ config/sketchybar/plugins/slack.sh
@@ -4,18 +4,21 @@ source "$CONFIG_DIR/colors.sh"
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Slack")
 
-LABEL_DRAWING=on
+ICON_DRAWING=on
 ICON_PADDING_LEFT=7
 ICON_PADDING_RIGHT=7
+LABEL_DRAWING=on
 LABEL_PADDING_LEFT=4
 LABEL_PADDING_RIGHT=7
+BACKGROUND_DRAWING=on
 
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
 
     if [[ $LABEL == "" ]]; then
-        ICON_COLOR="$GREEN"
+        ICON_DRAWING=off
         LABEL_DRAWING=off
+        BACKGROUND_DRAWING=off
     elif [[ $LABEL == "•" ]]; then
         ICON_COLOR="$YELLOW"
         ICON_PADDING_RIGHT=0
@@ -31,6 +34,7 @@ else
 fi
 
 slack=(
+  icon.drawing="$ICON_DRAWING"
   icon.color="$ICON_COLOR"
   icon.padding_left="$ICON_PADDING_LEFT"
   icon.padding_right="$ICON_PADDING_RIGHT"
@@ -38,5 +42,6 @@ slack=(
   label.drawing="$LABEL_DRAWING"
   label.padding_left="$LABEL_PADDING_LEFT"
   label.padding_right="$LABEL_PADDING_RIGHT"
+  background.drawing="$BACKGROUND_DRAWING"
 )
 sketchybar --set "$NAME" "${slack[@]}"
