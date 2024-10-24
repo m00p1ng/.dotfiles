0c3d2d841b53882c0159af40999d0ef513792166
m00p1ng <mongkonchai4412@gmail.com>
2024-10-24 13:43:26 +0700

    fix shell

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -135,6 +135,10 @@ in {
 
         set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'                                                         # undercurl support
         set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+
+        # bug shell: https://github.com/nix-community/home-manager/issues/5952
+        set -gu default-command
+        set -g default-shell "$SHELL"
       '';
     };
 
