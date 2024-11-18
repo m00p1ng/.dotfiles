6b7d83e752b41ec36748f3d3a19bd85d54aa6f34
m00p1ng <mongkonchai4412@gmail.com>
2024-11-18 20:05:35 +0700

    add tmux auto attach

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -150,5 +150,26 @@ in {
       source = ../../config/tmux;
       recursive = true;
     };
+
+    programs.fish = {
+      interactiveShellInit = ''
+        # auto attach tmux configuration
+        if not set -q TMUX
+          set tmux_default_session_name workspace
+          set tmux_attached_client (tmux list-clients 2> /dev/null | grep $tmux_default_session_name | wc -l)
+          set tmux_has_default_session (tmux ls 2> /dev/null | grep $tmux_default_session_name | wc -l)
+
+          if [ $tmux_attached_client -eq 0 ]
+            if [ $tmux_has_default_session -eq 0 ]
+              tmux new -s $tmux_default_session_name
+            else
+              tmux a -t $tmux_default_session_name
+            end
+          else
+            tmux
+          end
+        end
+      '';
+    };
   };
 }
