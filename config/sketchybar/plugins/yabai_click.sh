e5a53696d807d6fd58cf3c61b896c831f2d8b572
m00p1ng <mongkonchai4412@gmail.com>
2024-11-27 10:41:24 +0700

    update

--- config/sketchybar/plugins/yabai_click.sh
+++ config/sketchybar/plugins/yabai_click.sh
@@ -7,29 +7,18 @@ mode=$(yabai -m query --spaces --display | jq -r 'map(select(."has-focus" == tru
 NEXT_MODE=bsp
 ICON="$YABAI_GRID"
 
-# case "$mode" in
-#   bsp)
-#     NEXT_MODE=stack
-#     ICON="$YABAI_STACK"
-#   ;;
-#   stack)
-#     NEXT_MODE=float
-#     ICON="$YABAI_FLOAT"
-#   ;;
-#   float)
-#     NEXT_MODE=bsp
-#     ICON="$YABAI_GRID"
-#   ;;
-# esac
-
 case "$mode" in
-  bsp)
-    NEXT_MODE=float
-    ICON="$YABAI_FLOAT"
+bsp)
+  NEXT_MODE=float
+  ICON="$YABAI_FLOAT"
   ;;
-  stack | float)
-    NEXT_MODE=bsp
-    ICON="$YABAI_GRID"
+stack)
+  NEXT_MODE=bsp
+  ICON="$YABAI_FLOAT"
+  ;;
+float)
+  NEXT_MODE=bsp
+  ICON="$YABAI_GRID"
   ;;
 esac
 
