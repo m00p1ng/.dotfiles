d5e04714e993ede64e3e238e68dfe9bc79f07194
m00p1ng <mongkonchai4412@gmail.com>
2025-01-06 22:50:34 +0700

    move to use uv

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -1,10 +1,24 @@
 {
-  programs.direnv = {
-    nix-direnv.enable = true;
-    config = {
-      global = {
-        hide_env_diff = true;
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.programs.direnv;
+in {
+  config = mkIf cfg.enable {
+    programs.direnv = {
+      nix-direnv.enable = true;
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
       };
+    };
+
+    xdg.configFile.direnv = {
+      source = ../../config/direnv;
+      recursive = true;
     };
   };
 }
--- modules/home-manager/python.nix
+++ modules/home-manager/python.nix
@@ -30,50 +30,10 @@ in {
 
   config = mkIf cfg.enable (mkMerge [
     {
-      home.packages = [
+      home.packages = with pkgs; [
         pythonEnv
+        uv
       ];
-
-      # ref: https://gist.github.com/tommyip/cf9099fa6053e30247e5d0318de2fb9e
-      xdg.configFile."fish/conf.d/venv.fish".text = ''
-        function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-          status --is-command-substitution; and return
-
-          # Check if we are inside a git directory
-          if git rev-parse --show-toplevel &>/dev/null
-            set gitdir (realpath (git rev-parse --show-toplevel))
-            set cwd (pwd -P)
-            # While we are still inside the git directory, find the closest
-            # virtualenv starting from the current directory.
-            while string match "$gitdir*" "$cwd" &>/dev/null
-        ${optionalString cfg.poetry.enable ''
-          set poetry_path (poetry env info --path 2>/dev/null)
-          if test -n "$poetry_path"
-            source "$poetry_path/bin/activate.fish" &>/dev/null
-            return
-          end
-        ''}
-
-              if test -e "$cwd/.venv/bin/activate.fish"
-                source "$cwd/.venv/bin/activate.fish" &>/dev/null
-                return
-              else
-                set cwd (path dirname "$cwd")
-              end
-            end
-          end
-          # If virtualenv activated but we are not in a git directory, deactivate.
-          if test -n "$VIRTUAL_ENV" && type -q deactivate
-            deactivate
-          end
-        end
-      '';
     }
-
-    (mkIf cfg.poetry.enable {
-      programs.poetry = {
-        enable = true;
-      };
-    })
   ]);
 }
