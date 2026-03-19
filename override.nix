5fe8fc98d9f9c8a9ec75d59f08747fd2279a26ec
m00p1ng <mongkonchai4412@gmail.com>
2026-03-19 23:16:07 +0700

    update

--- override.nix
+++ override.nix
@@ -7,15 +7,13 @@
 with lib; {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
-      ffmpeg
-      nmap
     ];
 
     programs.git = {
       settings = {
         user = {
-          name = "m00p1ng";
-          email = "mongkonchai4412@gmail.com";
+          name = ;
+          email = ;
         };
       };
     };
