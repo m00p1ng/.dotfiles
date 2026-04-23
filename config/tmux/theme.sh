78a9f8a0a13b17a42a10cab3037774dee688ae6f
m00p1ng <mongkonchai4412@gmail.com>
2026-04-24 05:06:06 +0700

    refactor(tmux): inline theme.sh into nix configuration

--- config/tmux/theme.sh
+++
@@ -1,9 +0,0 @@
-set_automatic_rename_format() {
-  is_nvim="#{==:#{pane_current_command},nvim}"
-  is_fish="#{==:#{pane_current_command},fish}"
-  path_format="#{?#{==:#{pane_current_path},#{HOME}},~ (#{pane_current_command}),#{b:pane_current_path}}"
-
-  echo "#{?#{||:$is_nvim,$is_fish},$path_format,#{pane_current_command}}"
-}
-
-tmux set -g automatic-rename-format "$(set_automatic_rename_format)"
--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -179,9 +179,18 @@ in {
       run-shell ${config.xdg.configHome}/tmux/theme.sh
     '';
 
-    xdg.configFile."tmux" = {
-      source = ../../config/tmux;
-      recursive = true;
-    };
+    xdg.configFile."tmux/theme.sh".text =
+      #sh
+      ''
+        set_automatic_rename_format() {
+          is_nvim="#{==:#{pane_current_command},nvim}"
+          is_fish="#{==:#{pane_current_command},fish}"
+          path_format="#{?#{==:#{pane_current_path},#{HOME}},~ (#{pane_current_command}),#{b:pane_current_path}}"
+
+          echo "#{?#{||:$is_nvim,$is_fish},$path_format,#{pane_current_command}}"
+        }
+
+        tmux set -g automatic-rename-format "$(set_automatic_rename_format)"
+      '';
   };
 }
