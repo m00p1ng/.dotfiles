88bb4358bd4bb131a59af90e6bb08eabdc6b144f
m00p1ng <mongkonchai4412@gmail.com>
2026-04-23 20:13:32 +0700

    git: add aliases repo/pr/pr-web using gh CLI

--- modules/home-manager/gh.nix
+++ modules/home-manager/gh.nix
@@ -1,7 +1,24 @@
 {
-  programs.gh = {
-    settings = {
-      version = "1";
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.programs.gh;
+in {
+  config = mkIf cfg.enable {
+    programs.gh = {
+      settings = {
+        version = "1";
+      };
+    };
+
+    programs.git = {
+      settings = {
+        alias = {
+          pr = "!gh pr create --web";
+        };
+      };
     };
   };
 }
