ad497a06288c77540dcc31323e8fc4a5eb9f2def
m00p1ng <mongkonchai4412@gmail.com>
2026-07-26 15:41:21 +0700

    fix(nix): disable rtk checks to unblock build
    rtk 0.43.0 has dead code that fails -D warnings in the test binary.
    Skip doCheck until fixed upstream.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,11 @@
   nixpkgs.config.packageOverrides = pkgs: {
     icalBuddy = pkgs.callPackage ./icalBuddy/package.nix {};
     icalPal = pkgs.callPackage ./icalPal/package.nix {};
+
+    # rtk 0.43.0 upstream has dead code that fails `-D warnings` when
+    # compiling the test binary. Skip checks until fixed upstream.
+    rtk = pkgs.rtk.overrideAttrs (_: {
+      doCheck = false;
+    });
   };
 }
